8542f9f8a71cc16dbb7039ae09314cb4e141b55b
TesseractCat <tesseractcats@gmail.com>
2023-04-13 20:26:39 +0000

    Slight build modifications

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-bash version.sh
 git pull
+bash version.sh
 sudo pkill BG3D
 npm run build
 cargo build --release
-sudo ./target/release/BG3D 443 > log.txt &
+sudo ./target/release/BG3D 9095 > log.txt &
