ad5de5f6cdc93880722e8df0e464d896f7f55e1a
Tesseract Cat <tesseractcats@gmail.com>
2024-09-04 15:22:44 -0400

    Fix cache busting

--- version.sh
+++ version.sh
@@ -1,2 +1,5 @@
 hash=$(git describe --always)
 sed "s/{{version}}/$hash/g" templates/index.html > static/index.html
+sed "s/{{version}}/$hash/g" templates/index.css > static/index.css
+sed "s/{{version}}/$hash/g" templates/frontpage.html > static/frontpage/index.html
+sed "s/{{version}}/$hash/g" templates/frontpage.css > static/frontpage/index.css
