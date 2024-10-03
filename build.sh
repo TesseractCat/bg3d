59beab6deeb1518a3f52fa4a9a579bec623f6274
tesseractcat <tesseractcats@gmail.com>
2024-10-03 19:53:05 +0000

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 git pull
 bash version.sh
 pkill BG3D
 npm install
 npm run build
 cargo build --release
-./target/release/BG3D https://birdga.me:9095 > log.txt &
+./target/release/BG3D https://birdga.me:9095 &> log.txt &
