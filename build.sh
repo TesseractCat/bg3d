ea12a7af3177b514864ccd249ddecb2244c57529
tesseractcat <tesseractcats@gmail.com>
2024-11-21 18:39:55 +0000

    Use systemd

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+systemctl --user stop bg3d
 git pull
 bash version.sh
 pkill BG3D
 npm install
 npm run build
 cargo build --release
-./target/release/BG3D https://birdga.me:9095 &> log.txt &
+systemctl --user start bg3d
+#./target/release/BG3D https://birdga.me:9095 &> log.txt &
